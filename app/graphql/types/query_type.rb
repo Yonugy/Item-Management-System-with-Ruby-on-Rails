54ff35d83598e9dfb8657e6ea3bbb623489dfa37
Yong Wai <yongwaichong@gmail.com>
2025-04-09 02:17:26 +0800

    Filter and Search feature

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -19,12 +19,52 @@ module Types
     end
 
     # Add a field for fetching items
-    field :items, [Types::ItemType], null: false, description: "Fetches a list of all items." do
+    # field :items, [Types::ItemType], null: false, description: "Fetches a list of all items." do
+    # end
+
+    field :items, [Types::ItemType], null: false, description: "Fetch all items" do
+      argument :search, String, required: false, description: "Search term to filter items"
+      argument :filter_by, String, required: false, description: "Filter by either title or category"
+      argument :sort_by, String, required: false, default_value: 'created_at', description: "Field to sort by"
+      argument :sort_order, String, required: false, default_value: 'desc', description: "Sort order (asc or desc)"
     end
 
     # Resolver for fetching all items
-    def items
-      Item.all
+    def items(search: nil, filter_by:, sort_by:, sort_order:)
+      # query = Item.all
+
+      # # Implement Search
+      # if search.present?
+      #   query = query.where('title ILIKE ?', "%#{search}%")
+      # end
+
+      # # Implement Sorting
+      # if sort_order == 'desc'
+      #   query = query.order("#{sort_by} DESC")
+      # else
+      #   query = query.order("#{sort_by} ASC")
+      # end
+
+      # query
+      # Initialize the query on the `Item` model
+      query = Item.all
+
+      # Apply search filter if search term is provided
+      
+
+      if search.present?
+        if filter_by == "title"
+          query = query.where("title LIKE ?", "%#{search}%")
+        elsif filter_by == "category"
+          query = query.where("category LIKE ?", "%#{search}%")
+        end
+        # query = query.where('title LIKE ? OR category LIKE ?', "%#{search}%", "%#{search}%")
+      end
+
+      # Apply sorting based on the `sort_by` and `sort_order` arguments
+      query = query.order("#{sort_by} #{sort_order}")
+
+      query
     end
 
     # Add root-level fields here.
